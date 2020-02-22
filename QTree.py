98b3e9d364d0ffa56a9c5b5826206ea272e4d041
Pratik Tayshete <pratiktayshete1221@gmail.com>
2020-02-22 21:07:37 +0530

    Initial Commit

---
+++ QTree.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'QTree.ui'
+##
+## Created by: Qt User Interface Compiler version 5.14.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
+    QRect, QSize, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
+    QRadialGradient)
+from PySide2.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.gridLayout = QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.dataTreeWidget = QTreeWidget(self.centralwidget)
+        __qtreewidgetitem = QTreeWidgetItem()
+        __qtreewidgetitem.setText(0, u"1");
+        self.dataTreeWidget.setHeaderItem(__qtreewidgetitem)
+        self.dataTreeWidget.setObjectName(u"dataTreeWidget")
+
+        self.gridLayout.addWidget(self.dataTreeWidget, 0, 1, 1, 1)
+
+        self.treeButton = QPushButton(self.centralwidget)
+        self.treeButton.setObjectName(u"treeButton")
+
+        self.gridLayout.addWidget(self.treeButton, 0, 0, 1, 1)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.treeButton.setText(QCoreApplication.translate("MainWindow", u"View / Hide ", None))
+    # retranslateUi
+
